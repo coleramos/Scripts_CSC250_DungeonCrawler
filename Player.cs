c080ca4da2115cacf9d35bfa3cb53923c1a3e220
coleramos <cole.ramos@cuw.edu>
2024-02-12 11:21:18 -0600

    made changes

--- Player.cs
+++ Player.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
 
-public class Player : MonoBehaviour
+public class Player1Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Player thePlayer;
+    public TextMeshPro tm;
+    public GameObject destinationGO;
+    public float speed = 3.0f; //NEW CODE
+
     void Start()
     {
-        
+        this.thePlayer = new Player("Cole");
+        tm.text = this.thePlayer.getName() + "->" + this.thePlayer.getHP();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        this.thePlayer.display();
+        //NEW CODE**
+        //print(Vector3.Distance(this.gameObject.transform.position != this.destinationGO.transform.position));
+        if (Vector3.Distance(this.gameObject.transform.position, this.destinationGO.transform.position) > 1.0f)
+        {
+            this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.destinationGO.transform.position, this.speed * Time.deltaTime);
+        }
+        //NEW CODE**
     }
 }
