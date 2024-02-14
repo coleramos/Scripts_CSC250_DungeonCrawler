32076c74c0ad6839664afadc2bcc368379490be2
coleramos <cole.ramos@cuw.edu>
2024-02-14 12:16:08 -0600

    updated

--- SphereScript.cs
+++ SphereScript.cs
@@ -7,8 +7,7 @@ public class SphereScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("inside Sphere Script");
-        print("Secret Number = " + MySingleton.secretNumber);
+
 
     }
 
