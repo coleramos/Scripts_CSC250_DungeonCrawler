bcccfb665564f8f634dfe1226b6be222077db7a5
coleramos <cole.ramos@cuw.edu>
2024-02-21 11:58:10 -0600

    updated

--- MySingleton.cs
+++ MySingleton.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 public class MySingleton
 {
     public static string currentDirection = "?";
+    public static Room[] theRooms = new Room[100];
+    public static int numRooms = 0;
+
+
+    public static void addRoom(Room r)
+    {
+        theRooms[numRooms] = r;
+        numRooms++;
+    }
 }
